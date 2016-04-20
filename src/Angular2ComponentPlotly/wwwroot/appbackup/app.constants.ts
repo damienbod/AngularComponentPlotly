dc69531657d3f6ac43027e54aa8b207a6826c3b3
damienbod <damien_bod@hotmail.com>
2016-04-20 21:25:11 +0200

    deleting unused files

--- src/Angular2ComponentPlotly/wwwroot/appbackup/app.constants.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from 'angular2/core';
-
-@Injectable()
-export class Configuration {
-    public Server: string = "http://localhost:31647/";
-}
--- src/Angular2ComponentPlotly/wwwroot/appbackup/models/BarTrace.ts
+++
@@ -1,3 +0,0 @@
-﻿export class BarTrace {
-    Y: number[];
-}
