cb3f84220de010caccd67b58ccc01c55e0d2a3ae
damienbod <damien_bod@hotmail.com>
2016-05-06 17:00:32 +0200

    adding the web api client

--- src/angular2plotly/src/app/snake-data-service.service.ts
+++ src/angular2plotly/src/app/snake-data-service.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map'
+import { Observable } from 'rxjs/Observable';
+import { Configuration } from './app.constants';
+import { GeographicalRegion } from './models/GeographicalRegion';
+import { GeographicalCountries } from './models/GeographicalCountries';
 
 @Injectable()
 export class SnakeDataServiceService {
 
-  constructor() {}
+    private actionUrl: string;
+    private headers: Headers;
+  
+    constructor(private _http: Http, private _configuration: Configuration) {
+        this.actionUrl = `${_configuration.Server}api/SnakeData/`;   
+    }
 
+    private setHeaders() {
+        this.headers = new Headers();
+        this.headers.append('Content-Type', 'application/json');
+        this.headers.append('Accept', 'application/json');
+    }
+
+    public GetGeographicalRegions = (): Observable<GeographicalRegion[]> => {
+        this.setHeaders();
+        return this._http.get(`${this.actionUrl}GeographicalRegions`, {
+            headers: this.headers
+        }).map(res => res.json());
+    }
+
+    public GetRegionBarChartData = (region: string): Observable<GeographicalCountries> => {
+        this.setHeaders();
+        return this._http.get(`${this.actionUrl}RegionBarChart/${region}`, {
+            headers: this.headers
+        }).map(res => res.json());
+    }
 }
