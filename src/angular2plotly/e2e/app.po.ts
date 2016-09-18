42e777047a9d2771d64769c7441d261bc343b657
damienbod <damien_bod@hotmail.com>
2016-09-18 10:43:40 +0200

    new empty cli project

--- src/angular2plotly/e2e/app.po.ts
+++ src/angular2plotly/e2e/app.po.ts
@@ -1,3 +1,5 @@
+import { browser, element, by } from 'protractor/globals';
+
 export class Angular2plotlyPage {
   navigateTo() {
     return browser.get('/');
--- src/angular2plotly/src/main.ts
+++ src/angular2plotly/src/main.ts
@@ -1,18 +1,12 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
+import './polyfills.ts';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { HTTP_PROVIDERS } from '@angular/http';
-import { provide } from '@angular/core';
-import { Angular2plotlyAppComponent, environment } from './app/';
-import { Configuration } from './app/app.constants';
-import { APP_ROUTER_PROVIDERS } from './app/app.routes';
+import { environment } from './environments/environment';
+import { AppModule } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(Angular2plotlyAppComponent, [
-    APP_ROUTER_PROVIDERS,
-    HTTP_PROVIDERS,
-    Configuration,
-]).catch(err => console.error(err));
-
+platformBrowserDynamic().bootstrapModule(AppModule);
