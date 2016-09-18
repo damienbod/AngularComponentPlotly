3475555f74277ae0fd21114b0b657fd5cb71898f
damienbod <damien_bod@hotmail.com>
2016-09-18 11:13:26 +0200

    updating overview

--- src/angular2plotly/src/app/app.component.ts
+++ src/angular2plotly/src/app/app.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from  '@angular/router';
+
+import { SnakeDataService } from './snake-data.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  directives: [ROUTER_DIRECTIVES],
+    providers: [
+        SnakeDataService
+    ]
+})
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'angular2plotly works!';
 }
