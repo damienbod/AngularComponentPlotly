421f26b9b615d377078fd2e7c09ce25c0dd62495
damienbod <damien_bod@hotmail.com>
2016-09-18 11:17:08 +0200

    adding the constants and removing the legacy angular2 rc code

--- src/angular2plotly/src/app/app.component.ts
+++ src/angular2plotly/src/app/app.component.ts
@@ -1,25 +1,13 @@
 import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-
-import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from  '@angular/router';
-
 import { SnakeDataService } from './snake-data.service';
 
 @Component({
-  moduleId: module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-  directives: [ROUTER_DIRECTIVES],
-    providers: [
-        SnakeDataService
-    ]
+  styleUrls: ['./app.component.css'],
+  providers: [
+		SnakeDataService
+  ]
 })
 
 export class AppComponent {
