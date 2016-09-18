bbcc166fa3c5b37d161cc79b9c24794945f920be
damienbod <damien_bod@hotmail.com>
2016-09-18 11:41:15 +0200

    updating region

--- src/angular2plotly/src/app/region/region.component.ts
+++ src/angular2plotly/src/app/region/region.component.ts
@@ -1,15 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Observable }  from 'rxjs/Observable';
+
+import { PlotlyComponent } from '../plotly/plotly.component';
+import { SnakeDataService } from '../snake-data.service';
+import { GeographicalRegion } from '../models/GeographicalRegion';
+import { GeographicalCountries } from '../models/GeographicalCountries';
+import { BarTrace } from '../models/BarTrace';
 
 @Component({
   selector: 'app-region',
   templateUrl: './region.component.html',
   styleUrls: ['./region.component.css']
 })
-export class RegionComponent implements OnInit {
 
-  constructor() { }
+export class RegionComponent implements OnInit, OnDestroy   {
 
-  ngOnInit() {
-  }
+    private id: number;
+    public message: string;
+    private sub: any;
+    public GeographicalCountries: GeographicalCountries;
 
+    private name: string;
+    public PlotlyLayout: any;
+    public PlotlyData: any;
+    public PlotlyOptions: any;
+
+    constructor(
+        private _snakeDataService: SnakeDataService,
+        private _route: ActivatedRoute,
+        private _router: Router
+    ) {
+        this.message = "region";
+    }
+
+	ngOnInit() {     
+
+        this.sub = this._route.params.subscribe(params => {
+            this.name = params['name']; 
+            if (!this.GeographicalCountries) {
+            this.getGetRegionBarChartData();
+        } 
+        });      
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    private getGetRegionBarChartData() {
+        console.log('RegionComponent:getData starting...');
+        this._snakeDataService
+            .GetRegionBarChartData(this.name)
+            .subscribe(data => this.setReturnedData(data),
+            error => console.log(error),
+            () => console.log('Get GeographicalCountries completed for region'));
+    }
+
+    private setReturnedData(data: any) {
+        this.GeographicalCountries = data;
+        this.PlotlyLayout = {
+            title: this.GeographicalCountries.RegionName + ": Number of snake bite deaths",
+            height: 500,
+            width: 1200
+        };
+
+        this.PlotlyData = [
+            {
+                x: this.GeographicalCountries.X,
+                y: this.getYDatafromDatPoint(),
+                name: "Number of snake bite deaths",
+                type: 'bar',
+                orientation: 'v'
+            }
+        ];
+
+        console.log("recieved plotly data");
+        console.log(this.PlotlyData);
+    }
+
+    private getYDatafromDatPoint() {
+        return this.GeographicalCountries.NumberOfDeathsHighData.Y;
+    }
 }
