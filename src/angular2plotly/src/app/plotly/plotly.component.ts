c5ae49cad40e8de257159281cd3e4aa5e92ebd06
damienbod <damien_bod@hotmail.com>
2016-09-18 11:20:03 +0200

    updating plotly compoment

--- src/angular2plotly/src/app/plotly/plotly.component.ts
+++ src/angular2plotly/src/app/plotly/plotly.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, ElementRef} from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import { Router } from  '@angular/router';
+
+declare var Plotly: any;
 
 @Component({
-  selector: 'app-plotly',
-  templateUrl: './plotly.component.html',
+  selector: 'plotlychart',
+  template: `
+<div style="margin-bottom:100px;">
+    <div id="myPlotlyDiv"
+         name="myPlotlyDiv"
+         style="width: 480px; height: 400px;">
+        <!-- Plotly chart will be drawn inside this DIV -->
+    </div>
+</div>
+
+<div *ngIf="displayRawData">
+    raw data:
+    <hr />
+    <span>{{data | json}}</span>
+    <hr />
+    layout:
+    <hr />
+    <span>{{layout | json}}</span>
+    <hr />
+</div>
+`,
   styleUrls: ['./plotly.component.css']
 })
+
+
 export class PlotlyComponent implements OnInit {
 
-  constructor() { }
+    @Input() data: any;
+    @Input() layout: any;
+    @Input() options: any;
+    @Input() displayRawData: boolean;
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        console.log("ngOnInit PlotlyComponent");
+        console.log(this.data);
+        console.log(this.layout);
 
+        Plotly.newPlot('myPlotlyDiv', this.data, this.layout, this.options);
+    }
 }
